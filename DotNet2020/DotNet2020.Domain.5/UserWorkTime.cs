31a6cf17a9fc68db0cd5456a987471f83cfc4d04
ArtemS00 <arsol.plex@google.com>
2020-03-01 14:42:02 +0300

    Added Domain.5

---
+++ DotNet2020/DotNet2020.Domain.5/UserWorkTime.cs
@@ -0,0 +1,23 @@
+﻿using DotNet2020.Data;
+using System;
+
+namespace DotNet2020.Domain._5
+{
+    public class UserWorkTime
+    {
+        public AppIdentityUser User { get; protected set; }
+        public int EstimatedTime { get; protected set; }
+        public DateTime DateFrom { get; protected set; }
+        public DateTime DateTo { get; }
+
+        public UserWorkTime(AppIdentityUser user, int estimatedTime, DateTime dateFrom, DateTime dateTo)
+        {
+            User = user;
+            EstimatedTime = estimatedTime;
+            DateFrom = dateFrom;
+            DateTo = dateTo;
+        }
+
+        protected UserWorkTime() : base() { }
+    }
+}
