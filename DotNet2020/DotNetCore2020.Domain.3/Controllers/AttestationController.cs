0ec6d7edef6167a5c8c2e377718688bf384882b6
ArlearD <azelird@gmail.com>
2020-03-12 14:15:31 +0300

    Added initial project

---
+++ DotNet2020/DotNetCore2020.Domain.3/Controllers/AttestationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore2020.Domain._3.Controllers
+{
+    public class AttestationController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        public IActionResult Test2()
+        {
+            return View();
+        }
+    }
+}
