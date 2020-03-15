c73aafe3ee961bc51e260fcd0da2b62b2a26a6a1
ArtemS00 <arsol.plex@google.com>
2020-03-15 14:07:49 +0300

    [Added] Localization files, Entites

---
+++ DotNet2020/DotNet2020.Domain.5/Entities/UserWorkTime.cs
@@ -0,0 +1,21 @@
+﻿using DotNet2020.Data;
+using System;
+
+namespace DotNet2020.Domain._5.Entities
+{
+    public class UserWorkTime
+    {
+        public AppIdentityUser User { get; private set; }
+        public int EstimatedTime { get; set; }
+        public DayOfWeek DayOfWeek { get; set; }
+
+        public UserWorkTime(AppIdentityUser user, int estimatedTime, DayOfWeek dayOfWeek)
+        {
+            User = user;
+            EstimatedTime = estimatedTime;
+            DayOfWeek = dayOfWeek;
+        }
+
+        protected UserWorkTime() : base() { }
+    }
+}
