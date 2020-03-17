a2d7334215e20ae60706f727908dbb47577972c5
Hernantos <sattarov-artur@mail.ru>
2020-03-17 18:30:07 +0300

    create db instances

---
+++ DotNet2020/DotNet2020.Domain.6/Models/FunctioningCapacityProject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet2020.Domain._6.Models
+{
+    class FunctioningCapacityProject
+    {
+        public int Id { get; protected set; }
+        public int ProjectId { get; protected set; }
+        public int PeriodId { get; protected set; }
+        public int FunctioningCapacity { get; protected set; }
+    }
+}
