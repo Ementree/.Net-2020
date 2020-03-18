a1adc72f1f1f1676f111d220ccd317494c050d4e
ArtemS00 <arsol.plex@google.com>
2020-03-18 20:04:22 +0300

    Fixed models

--- DotNet2020/DotNet2020.Domain.5/Entities/UserSkipTime.cs
+++ DotNet2020/DotNet2020.Domain.5/Entities/UserSkipTime.cs
@@ -1,7 +1,5 @@
 ﻿using DotNet2020.Data;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace DotNet2020.Domain._5.Entities
 {
--- DotNet2020/DotNet2020.Domain.5/Entities/UserWorkTime.cs
+++ DotNet2020/DotNet2020.Domain.5/Entities/UserWorkTime.cs
@@ -8,12 +8,14 @@ namespace DotNet2020.Domain._5.Entities
         public AppIdentityUser User { get; private set; }
         public int EstimatedTime { get; set; }
         public DayOfWeek DayOfWeek { get; set; }
+        public bool IsVisible { get; set; }
 
-        public UserWorkTime(AppIdentityUser user, int estimatedTime, DayOfWeek dayOfWeek)
+        public UserWorkTime(AppIdentityUser user, int estimatedTime, DayOfWeek dayOfWeek, bool isVisible)
         {
             User = user;
             EstimatedTime = estimatedTime;
             DayOfWeek = dayOfWeek;
+            IsVisible = isVisible;
         }
 
         protected UserWorkTime() : base() { }
