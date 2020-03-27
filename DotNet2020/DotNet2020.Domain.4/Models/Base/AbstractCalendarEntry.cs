e0cd0ebc57424cb08e2a5cb99952343b708ff76e
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-03-27 17:27:00 +0300

    Exception text

--- DotNet2020/DotNet2020.Domain.4/Models/Base/AbstractCalendarEntry.cs
+++ DotNet2020/DotNet2020.Domain.4/Models/Base/AbstractCalendarEntry.cs
@@ -12,7 +12,8 @@ namespace DotNet2020.Domain._4.Models
 
         public void ChangeDate(DateTime from, DateTime to)
         {
-            if (to < from) throw new ArgumentException();
+            if (to < from) throw new ArgumentException("You are trying set incorrect data period!" +
+                "'From' parametr should be less than 'To' or equal to them");
             From = from;
             To = to;
         }
