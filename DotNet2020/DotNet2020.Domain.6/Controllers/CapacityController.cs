0897a10dc10349a2740034f1e8cc1d088f8aa8f0
Ementree <47636110+Ementree@users.noreply.github.com>
2020-04-05 14:19:16 +0300

    raw view

--- DotNet2020/DotNet2020.Domain.6/Controllers/CapacityController.cs
+++ DotNet2020/DotNet2020.Domain.6/Controllers/CapacityController.cs
@@ -20,11 +20,13 @@ namespace DotNet2020.Domain._6.Controllers
         public IActionResult Index(int year = 2020)
         {
             var resources = context.Set<Resource>()
-                .Include(x => x.ResourceGroupType);
+                .Include(x => x.ResourceGroupType)
+                .ToList();
             var resourceCapacities = context.Set<ResourceCapacity>()
                 .Include(x => x.Resource)
                 .Include(x => x.Period)
-                .Where(x => x.Period.Start.Year == year);
+                .Where(x => x.Period.Start.Year == year)
+                .ToList();
 
             var viewModelList = new List<ViewModelCapacity>();
             var capacities = resourceCapacities
@@ -37,12 +39,17 @@ namespace DotNet2020.Domain._6.Controllers
                 .ToDictionary(pair => pair.key, pair => pair.value);
             foreach (var resource in resources)
             {
+                var capacityDic = new Dictionary<int, int>();
+                if (capacities.ContainsKey(resource.Id))
+                {
+                    capacityDic = capacities[resource.Id];
+                }
                 viewModelList.Add(new ViewModelCapacity(
                     resource.Id, 
                     resource.FirstName + ' ' + resource.LastName, 
                     resource.ResourceGroupType.Type, 
                     resource.ResourceGroupType.Group,
-                    capacities[resource.Id]));
+                    capacityDic));
             }
 
             var model = new Dictionary<string, List<ViewModelCapacity>>();
@@ -57,9 +64,36 @@ namespace DotNet2020.Domain._6.Controllers
                     model.Add(viewModel.Group, new List<ViewModelCapacity>() {viewModel});
                 }
             }
-            
 
+            ViewBag.Months = GetMonths(year);
+            ViewBag.MaxLengthName = GetMaxLengthName(model);
             return View(model);
+        }
+
+        private int GetMaxLengthName(Dictionary<string, List<ViewModelCapacity>> model)
+        {
+            var max = 0;
+            foreach (var resources in model)
+            {
+                var tmax = resources.Value.Max(x => x.Name.Length);
+                if (tmax > max) max = tmax;
+            }
+
+            return max;
+        }
+        private List<string> GetMonths(int year)
+        {
+            var months = new List<string>()
+            {
+                "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь",
+                "Декабрь"
+            };
+            for (var i = 0; i < 12; i++)
+            {
+                months[i] += " " + year;
+            }
+
+            return months;
         }
     }
 }
