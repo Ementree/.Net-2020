cab1ca7cc130c1215ba775451dc187a135ed851a
DmitriiPodlev <42978023+DmitriiPodlev@users.noreply.github.com>
2020-04-11 14:20:07 +0300

    ADAS-96

---
+++ DotNet2020/DotNet2020.Domain.5/Models/CreateReport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet2020.Domain._5.Models
+{
+    public class CreateReport
+    {
+        public string ReportName { get; set; }
+        public string Project { get; set; }
+        public string IssueFilter { get; set; }
+    }
+}
