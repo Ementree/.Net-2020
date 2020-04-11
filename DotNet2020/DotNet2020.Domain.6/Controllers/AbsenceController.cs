fbe85871fb3156d152cdb66c2e5e92b435115ca1
Egor <giviruk@gmail.com>
2020-04-11 21:45:49 +0300

    a

--- DotNet2020/DotNet2020.Domain.6/Controllers/AbsenceController.cs
+++ DotNet2020/DotNet2020.Domain.6/Controllers/AbsenceController.cs
@@ -20,28 +20,87 @@ namespace DotNet2020.Domain._6.Controllers
             var viewAbsences = new Absences();
 
             var absences = new List<CalendarEntry>();
+            absences.Add(new CalendarEntry() { UserName = "User ",From = new System.DateTime(2020,06,08),To = new System.DateTime(2020,6,15)});
+            absences.Add(new CalendarEntry() { UserName = "User 2", From = new System.DateTime(2020, 05, 02), To = new System.DateTime(2020, 5, 15) });
+            absences.Add(new CalendarEntry() { UserName = "User 3", From = new System.DateTime(2020, 06, 05), To = new System.DateTime(2020, 7, 15) });
+            absences.Add(new CalendarEntry() { UserName = "User ", From = new System.DateTime(2020, 06, 05), To = new System.DateTime(2020, 7, 15) });
+            absences.Add(new CalendarEntry() { UserName = "User ", From = new System.DateTime(2020, 04, 06), To = new System.DateTime(2020, 6, 15) });
 
-            for(int i = 0;i < 6; i++)
-            {
-                absences.Add(new CalendarEntry() { UserName = "User " + i.ToString() });
-            }
+
 
             var periods = context.Set<Period>().ToList();
+            
             viewAbsences.Periods = periods;
 
             var resourceAbsences = new Dictionary<string, List<int>>();
+            var periodNum = 0;
 
-            foreach(var absence in absences)
+            var countAbs = new List<int>();
+            foreach(var per in periods)
             {
-                var countAbs = new List<int>();
-                foreach(var per in periods)
+                countAbs.Add(0);
+            }
+            foreach (var period in periods)
+            {
+                foreach (var absence in absences)
                 {
-                    countAbs.Add(0);
+                    if (!resourceAbsences.ContainsKey(absence.UserName))
+                    {
+                        resourceAbsences.Add(absence.UserName, new List<int>(countAbs));
+                    }
+                    if (!Contains(period, absence))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        resourceAbsences[absence.UserName][periodNum] += (CalculateAbsences(period, absence));
+                    }
                 }
-                resourceAbsences.Add(absence.UserName, countAbs);
+                periodNum++;
             }
             viewAbsences.ResourceAbsences = resourceAbsences;
             return View(viewAbsences);
+        }
+
+        private int CalculateAbsences(Period period,CalendarEntry entry)
+        {
+            if (!Contains(period, entry))
+            {
+                return 0;
+            }
+
+            if (entry.From > period.Start && entry.To < period.End)
+            {
+                return entry.To.Day - entry.From.Day;
+            }
+            else
+            {
+                if (entry.From < period.Start && entry.To > period.End)
+                {
+                    return period.End.Day - period.Start.Day;
+                }
+                else
+                {
+                    if (entry.From < period.Start && entry.To < period.End)
+                    {
+                        return entry.To.Day - period.Start.Day;
+                    }
+                    else
+                    {
+                        if (entry.From > period.Start && entry.To > period.End)
+                        {
+                            return period.End.Day - entry.From.Day;
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
+
+        private bool Contains(Period period,CalendarEntry entry)
+        {
+            return entry.To > period.Start && entry.From < period.End;
         }
     }
 
