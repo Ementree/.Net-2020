a1016788691ba12e35692ef51db52ce00aed0fa1
DmitriiPodlev <42978023+DmitriiPodlev@users.noreply.github.com>
2020-04-11 17:38:48 +0300

    UpdateAdas-96

--- DotNet2020/DotNet2020.Domain.5/Models/CreateReport.cs
+++ DotNet2020/DotNet2020.Domain.5/Models/CreateReport.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DotNet2020.Domain._5.Models
 {
     public class CreateReport
     {
+        [Required] 
+        [MaxLength(50)]
         public string ReportName { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Project { get; set; }
+        [Required]
+        [MaxLength(1000)]
         public string IssueFilter { get; set; }
     }
 }
