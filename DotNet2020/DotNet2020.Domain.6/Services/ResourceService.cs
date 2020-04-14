35a903a0262b9c71d264125f324a5eac8004ae84
PrepConcede <tagmirs_letter@mail.ru>
2020-04-14 20:01:55 +0300

    pure view

---
+++ DotNet2020/DotNet2020.Domain.6/Services/ResourceService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DotNet2020.Domain._6.Models;
+
+namespace DotNet2020.Domain._6.Services
+{
+    public class ResourceService
+    {
+        public class ResourceToSelect
+        {
+            public int Id { get; protected set; }
+            public string FullName{ get; protected set; }
+
+            public ResourceToSelect(int id, string fullName)
+            {
+                Id = id;
+                FullName = fullName;
+            }
+        }
+
+        public static List<ResourceToSelect> GetResources()
+        {
+            return Enumerable.Range(1, 7)
+                .Select(item => new ResourceToSelect(item, $"Name LastName {item}"))
+                .ToList();
+        }
+    }
+}
