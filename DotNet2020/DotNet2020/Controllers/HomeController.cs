7d7b01f8a26c60e05d2f2557d23ce816be864a63
Nesom <rakhimovsamatarrr@gmail.com>
2020-04-15 17:19:24 +0300

    added report view

--- DotNet2020/DotNet2020/Controllers/HomeController.cs
+++ DotNet2020/DotNet2020/Controllers/HomeController.cs
@@ -19,18 +19,6 @@ namespace DotNet2020.Controllers
             _logger = logger;
         }
 
-        public IActionResult Report()
-        {
-            var reports = new List<IssueTimeInfo>
-                {
-                    new IssueTimeInfo(10,7),
-                    new IssueTimeInfo(11, 8),
-                    new IssueTimeInfo(15,1),
-                    new IssueTimeInfo(20,19),
-                    new IssueTimeInfo(25, 7)
-                };
-            return View(reports);
-        }
         public IActionResult Index()
         {
             return View();
