c4527ac4a62f97403a255a422fab0909c78b9808
DmitriiPodlev <42978023+DmitriiPodlev@users.noreply.github.com>
2020-04-16 00:13:05 +0300

    5_ADAS-96

---
+++ DotNet2020/DotNet2020.Domain.5/Controllers/ReportController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNet2020.Domain._5.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DotNet2020.Domain._5.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly ILogger<ReportController> _logger;
+
+        public ReportController(ILogger<ReportController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult CreateReport()
+        {
+            return View();
+        }
+    }
+}
