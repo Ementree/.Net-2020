ce57f4cd30921b2f382653c0c8c6bc59294a8e72
Mimahmetova_Albina_11-808 <wildFlow14@gmail.com>
2020-04-16 23:08:17 +0300

    add Language Analyzer

---
+++ DotNet2020/DotNet2020.Domain.1/DotNet2020.Domain._1/CommentContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace DotNet2020.Domain._1
+{
+    internal class CommentContext
+    {
+        private readonly SyntaxNodeAnalysisContext nodeContext;
+        private readonly SyntaxTreeAnalysisContext treeContext;
+        private readonly bool IsNode;
+
+        internal CommentContext(SyntaxNodeAnalysisContext сontext)
+        {
+            nodeContext = сontext;
+            IsNode = true;
+        }
+
+        internal CommentContext(SyntaxTreeAnalysisContext сontext)
+        {
+            treeContext = сontext;
+            IsNode = false;
+        }
+
+        internal void ReportDiagnostic(Diagnostic diagnostic)
+        {
+            if (IsNode) nodeContext.ReportDiagnostic(diagnostic);
+            else treeContext.ReportDiagnostic(diagnostic);
+        }
+    }
+}
