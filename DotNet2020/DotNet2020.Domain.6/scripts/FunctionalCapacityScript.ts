7bb9fef47b76e275691bc0717f3d19c8022d538f
Hernantos <sattarov-artur@mail.ru>
2020-04-18 17:41:01 +0300

    add current month hilight

--- DotNet2020/DotNet2020.Domain.6/scripts/FunctionalCapacityScript.ts
+++ DotNet2020/DotNet2020.Domain.6/scripts/FunctionalCapacityScript.ts
@@ -1,7 +1,57 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    console.log("doom загрузился")
+﻿function getCurrentMonthName() {
+    var currentMonthNumber = new Date().getMonth();
+    console.log("текущий месяц " + currentMonthNumber);
 
-    let selector = <HTMLSelectElement> document.getElementById("changeYearSelector");
+    switch (currentMonthNumber) {
+        case 0:
+            return "январь";
+        case 1:
+            return "февраль";
+        case 2:
+            return "март";
+        case 3:
+            return "апрель";
+        case 4:
+            return "май";
+        case 5:
+            return "июнь";
+        case 6:
+            return "июль";
+        case 7:
+            return "август";
+        case 8:
+            return "сентябрь";
+        case 9:
+            return "октябрь";
+        case 10:
+            return "ноябрь";
+        case 11:
+            return "декабрь";
+    }
+}
+
+
+function PaintCurrentMonthColumn() {
+    let monthRow = <HTMLTableRowElement>document.getElementById("monthRow");
+    let monthCellsCollection = monthRow.cells;
+    let currentMonthName = getCurrentMonthName();
+
+    for (let i = 1; i < monthCellsCollection.length; i++) {
+        let cellText = monthCellsCollection[i].innerText;
+        let monthName = cellText.split(' ')[0].toLowerCase();
+
+        if (monthName == currentMonthName) {
+
+            monthCellsCollection[i].classList.add("current-month");
+            console.log("добвился класс :" + currentMonthName);
+            return;
+        }
+        console.log(monthName);
+    }
+}
+
+function AddYearChangeEvent() {
+    let selector = <HTMLSelectElement>document.getElementById("changeYearSelector");
 
     selector.addEventListener("change", function () {
         let form = <HTMLFormElement>document.getElementById("changeYearForm");
@@ -12,4 +62,16 @@
         window.location.search = ("?currentYear=" + value);
         console.log(window.location.search);
     });
+}
+
+function AddAccuracyChangeEvent() {
+
+} 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("doom загрузился")
+
+    PaintCurrentMonthColumn();
+    AddYearChangeEvent();
 });
--- DotNet2020/DotNet2020.Domain.6/wwwroot6/js/FunctionalCapacityScript.js
+++ DotNet2020/DotNet2020.Domain.6/wwwroot6/js/FunctionalCapacityScript.js
@@ -1,5 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("doom загрузился");
+function getCurrentMonthName() {
+    var currentMonthNumber = new Date().getMonth();
+    console.log("текущий месяц " + currentMonthNumber);
+    switch (currentMonthNumber) {
+        case 0:
+            return "январь";
+        case 1:
+            return "февраль";
+        case 2:
+            return "март";
+        case 3:
+            return "апрель";
+        case 4:
+            return "май";
+        case 5:
+            return "июнь";
+        case 6:
+            return "июль";
+        case 7:
+            return "август";
+        case 8:
+            return "сентябрь";
+        case 9:
+            return "октябрь";
+        case 10:
+            return "ноябрь";
+        case 11:
+            return "декабрь";
+    }
+}
+function PaintCurrentMonthColumn() {
+    var monthRow = document.getElementById("monthRow");
+    var monthCellsCollection = monthRow.cells;
+    var currentMonthName = getCurrentMonthName();
+    for (var i = 1; i < monthCellsCollection.length; i++) {
+        var cellText = monthCellsCollection[i].innerText;
+        var monthName = cellText.split(' ')[0].toLowerCase();
+        if (monthName == currentMonthName) {
+            monthCellsCollection[i].classList.add("current-month");
+            console.log("добвился класс :" + currentMonthName);
+            return;
+        }
+        console.log(monthName);
+    }
+}
+function AddYearChangeEvent() {
     var selector = document.getElementById("changeYearSelector");
     selector.addEventListener("change", function () {
         var form = document.getElementById("changeYearForm");
@@ -10,5 +54,12 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location.search = ("?currentYear=" + value);
         console.log(window.location.search);
     });
+}
+function AddAccuracyChangeEvent() {
+}
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("doom загрузился");
+    PaintCurrentMonthColumn();
+    AddYearChangeEvent();
 });
 //# sourceMappingURL=FunctionalCapacityScript.js.map
