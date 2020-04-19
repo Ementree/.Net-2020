7d9ddb29a8c83358ee0994a5fabc7bda09f8715e
PrepConcede <tagmirs_letter@mail.ru>
2020-04-19 16:01:38 +0300

    main view remake

--- DotNet2020/DotNet2020.Domain.6/Controllers/PlanController.cs
+++ DotNet2020/DotNet2020.Domain.6/Controllers/PlanController.cs
@@ -44,11 +44,20 @@ namespace DotNet2020.Domain._6.Controllers
                 {
                     var key = pair.Key;
                     var value = pair.Value.GroupBy(res => res.PeriodId)
-                        .ToDictionary(group => group.ToList().FirstOrDefault().Period, group => group.ToList());
+                        .ToDictionary(group => 
+                            group.ToList().FirstOrDefault().Period, 
+                            group => group.ToList())
+                        
+                        ;
+                    
                     return KeyValuePair.Create(key, value);
                 })
                 .ToDictionary(pair => pair.Key, pair => pair.Value);
 
+            var funcCapacitiesProject = 
+                context.Set<FunctioningCapacityProject>()
+                .ToList();
+            ViewBag.FunctioningCapacityProject = funcCapacitiesProject;
             return View(model: model);
         }
 
@@ -56,8 +65,10 @@ namespace DotNet2020.Domain._6.Controllers
         public bool AddProject([FromBody] ProjectViewModel viewModel)
         {
             var project = new Project(viewModel.Name, 5);
-            /*
-            context.Set<Project>().Add(project);
+
+            var projectEntity = context.Set<Project>().Add(project);
+            context.SaveChanges();
+            var projectId = projectEntity.Entity.Id;
             var periods = viewModel.Periods;
             var dbPeriods = context.Set<Period>();
             foreach (var period in periods)
@@ -65,22 +76,52 @@ namespace DotNet2020.Domain._6.Controllers
                 var periodDb = context.Set<Period>()
                     .FirstOrDefault(p =>
                         (p.Start.Year == period.Date.Year && p.Start.Month == period.Date.Month));
+                int periodId;
                 if (periodDb == default)
                 {
-                    var newPeriod = new Period(new DateTime(period.Date.Year, period.Date.Month, 1), 
+                    var newPeriod = new Period(new DateTime(period.Date.Year, period.Date.Month, 1),
                         new DateTime(period.Date.Year, period.Date.Month, 28));
                     var addRes = context.Set<Period>().Add(newPeriod);
-                    var saveChanges = context.SaveChanges();
-                    var id = addRes.Entity.Id;
+                    context.SaveChanges();
+                    periodId = addRes.Entity.Id;
+                }
+                else
+                {
+                    periodId = periodDb.Id;
                 }
 
+                var projectPeriodCapacity = period.Capacity == -1 ? 0 : period.Capacity;
+                var functioningCapacityProject = new FunctioningCapacityProject(projectId, periodId, projectPeriodCapacity);
+                var functioningCapacityResources = new List<FunctioningCapacityResource>();
                 foreach (var resourceCapacityViewModel in period.Resources)
                 {
-                    var fres = new FunctioningCapacityResource();
+                    if(resourceCapacityViewModel.Capacity==-1)
+                        continue;
+                    var fres = new FunctioningCapacityResource(projectId,
+                        resourceCapacityViewModel.Id,
+                        resourceCapacityViewModel.Capacity,
+                        periodId);
+                    functioningCapacityResources.Add(fres);
                 }
-                
+
+                if (functioningCapacityResources.Count == 0 && functioningCapacityProject.FunctioningCapacity == 0)
+                {
+                    continue;
+                }
+
+                if (functioningCapacityResources.Count == 0 && functioningCapacityProject.FunctioningCapacity > 0)
+                {
+                    context.Set<FunctioningCapacityProject>().Add(functioningCapacityProject);
+                }
+                else
+                {
+                    context.Set<FunctioningCapacityResource>().AddRange(functioningCapacityResources);
+                    context.Set<FunctioningCapacityProject>().Add(functioningCapacityProject);
+                }
+
+                context.SaveChanges();
             }
-*/
+
             return true;
         }
     }
