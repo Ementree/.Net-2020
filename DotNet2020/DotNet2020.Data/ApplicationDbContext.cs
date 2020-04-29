b97a72bc09c7f2a44ae79f1d06fc11fbd7c6f690
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-04-29 09:59:39 +0300

    Update ApplicationDbContext.cs

--- DotNet2020/DotNet2020.Data/ApplicationDbContext.cs
+++ DotNet2020/DotNet2020.Data/ApplicationDbContext.cs
@@ -41,19 +41,4 @@ namespace DotNet2020.Data
                     (AbsenceType.Illness);
         }
     }
-
-    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
-    {
-        public ApplicationDbContext CreateDbContext(string[] args)
-        {
-            IConfigurationRoot configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
-            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
-            builder.UseNpgsql(connectionString, b => b.MigrationsAssembly("DotNet2020.Data"));
-            return new ApplicationDbContext(builder.Options);
-        }
-    }
 }
