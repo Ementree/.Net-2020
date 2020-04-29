74448daa7a8268634549de97276cf55dbdaf8425
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-04-29 21:31:24 +0300

    заменяем на  DataAnnotations

--- DotNet2020/DotNet2020.Domain.4/Controllers/SickDayController.cs
+++ DotNet2020/DotNet2020.Domain.4/Controllers/SickDayController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using DotNet2020.Data;
 using DotNet2020.Domain._4.Models;
 using DotNet2020.Domain._4_.Models.ModelView;
+using DotNet2020.Domain.Filters;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -27,15 +28,18 @@ namespace DotNet2020.Domain._4.Controllers
         
         [HttpPost]
         [Authorize]
+        [ValidationFilter]
         public IActionResult Add(SickDayViewModel viewModel)
         {
-            #warning Используйте DataAnnotations аттрибуты
-            if (viewModel.Day == DateTime.MinValue)
-            {
-                ModelState.AddModelError("Error1", "Введите дату");
-                return View();
-            }
-            var sickDay = new SickDay(viewModel.Day, viewModel.Day,
+            // #warning Используйте DataAnnotations аттрибуты
+            // if (viewModel.Day == DateTime.MinValue)
+            // {
+            //     ModelState.AddModelError("Error1", "Введите дату");
+            //     return View();
+            // }
+            var sickDay = new SickDay(
+                viewModel.Day ?? throw new NullReferenceException(),
+                viewModel.Day ?? throw new NullReferenceException(),
                 _dbContext.Set<AppIdentityUser>().FirstOrDefault(u => u.Email == HttpContext.User.Identity.Name));
             _dbContext.Set<AbstractCalendarEntry>().Add(sickDay);
             _dbContext.SaveChanges();
