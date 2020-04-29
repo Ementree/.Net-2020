25aa27ceecd2298bb1cbd9f8e321e547e4090dbb
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-04-29 21:37:46 +0300

    Update VacationController.cs

--- DotNet2020/DotNet2020.Domain.4/Controllers/VacationController.cs
+++ DotNet2020/DotNet2020.Domain.4/Controllers/VacationController.cs
@@ -31,21 +31,21 @@ namespace DotNet2020.Domain._4.Controllers
         [Authorize]
         public IActionResult Add(VacationViewModel viewModel)
         {
-            var user = _dbContext.Set<AppIdentityUser>().FirstOrDefault(u => u.Id == User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var user = _dbContext.Set<AppIdentityUser>().FirstOrDefault(u => 
+                u.Id == User.FindFirstValue(ClaimTypes.NameIdentifier));
+            
             var days = DomainLogic.GetDatesFromInterval(
                 viewModel.From ?? throw new NullReferenceException(), 
                 viewModel.To ?? throw new NullReferenceException());
-            var hollidays = _dbContext.Set<Holiday>().Where(u => u.Date >= viewModel.From && u.Date <= viewModel.To).ToList();
+            
+            var hollidays = _dbContext.Set<Holiday>().Where(u => 
+                u.Date >= viewModel.From && u.Date <= viewModel.To).ToList();
+            
+            #warning Используйте DataAnnotations аттрибуты
             if (user.TotalDayOfVacation < DomainLogic.GetWorkDay(days, hollidays))
             {
                 ModelState.AddModelError("Error2", "Количество запрашеваемых дней отпуска превышает количество доступных вам");
                 return View(viewModel);
-            }
-            #warning Используйте DataAnnotations аттрибуты
-            if (viewModel.From == DateTime.MinValue && viewModel.From == DateTime.MinValue)
-            {
-                ModelState.AddModelError("Error1", "Введите даты");
-                return View();
             }
             if (viewModel.From >= viewModel.To)
             {
