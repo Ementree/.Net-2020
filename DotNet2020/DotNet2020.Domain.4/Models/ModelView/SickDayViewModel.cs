b5570858d7785b8225c2728c2c4271e3debee074
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-05-07 14:51:57 +0300

    Делаем валидацию сикдей на прошлую дату

--- DotNet2020/DotNet2020.Domain.4/Models/ModelView/SickDayViewModel.cs
+++ DotNet2020/DotNet2020.Domain.4/Models/ModelView/SickDayViewModel.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
+using DotNet2020.Domain.Attributes;
 
 namespace DotNet2020.Domain._4_.Models.ModelView
 {
     public class SickDayViewModel
     {
         [Required(ErrorMessage = "Введите дату")]
+        [LessThanCurrentDate(ErrorMessage = "SickDay можно брать только на прошедние даты")]
         public DateTime? Day { get; set; }
     }
 }
