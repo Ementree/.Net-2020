59869272f7b5a92711a4ee1baa3ac87fb5a8d162
Nesom <rakhimovsamatarrr@gmail.com>
2020-05-07 13:30:32 +0300

    added ienumerable

--- DotNet2020/DotNet2020.Domain.5/Models/ChartModel.cs
+++ DotNet2020/DotNet2020.Domain.5/Models/ChartModel.cs
@@ -1,26 +1,21 @@
 ﻿using DotNet2020.Domain._5.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DotNet2020.Domain._5.Models
 {
     public class ChartModel
     {
-        public Chart Chart1 { get; set; }
-        public Chart Chart2 { get; set; }
-        public Chart Chart3 { get; set; }
-        public Chart Chart4 { get; set; }
+        public List<Chart> Charts { get; set; }
         public Issue[] Issues { get; set; }
         public Report Report { get; set; }
         public ChartModel() { }
-        public ChartModel(Chart chart1, Chart chart2, Chart chart3, Chart chart4, Issue[] issuess)
+        public ChartModel(IEnumerable<Chart> charts, Issue[] issuess)
         {
             Issues = issuess;
-            Chart1 = chart1;
-            Chart2 = chart2;
-            Chart3 = chart3;
-            Chart4 = chart4;
+            Charts = charts.ToList();
         }
     }
 }
