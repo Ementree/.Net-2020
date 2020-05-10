15ee1248400e6e38dfd489471503df4680d7cf28
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-05-10 11:07:52 +0300

    Create EmployeeCalendar.cs

---
+++ DotNet2020/DotNet2020.Domain.4/Models/EmployeeCalendar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DotNet2020.Domain.Core.Models;
+
+namespace DotNet2020.Domain.Models
+{
+    public class EmployeeCalendar
+    {
+        public int Id { get; set; }
+        public Employee Employee { get; set; }
+
+        public int TotalDayOfVacation { get; set; }
+        public bool IsLastVacationApproved { get; private set; }
+
+        public void Approve()
+        {
+            IsLastVacationApproved = true;
+        }
+
+        public void Reject()
+        {
+            IsLastVacationApproved = false;
+        }
+    }
+}
