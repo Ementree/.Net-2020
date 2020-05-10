f239cd4fe7902425d15df860eb7888045cf085b2
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-05-10 16:53:35 +0300

    Update AbstractCalendarEntry.cs

--- DotNet2020/DotNet2020.Domain.4/Models/Base/AbstractCalendarEntry.cs
+++ DotNet2020/DotNet2020.Domain.4/Models/Base/AbstractCalendarEntry.cs
@@ -7,8 +7,8 @@ namespace DotNet2020.Domain._4.Models
     public abstract class AbstractCalendarEntry
     {
         public int Id { get; set; }
-        public DateTime From { get; protected set; }
-        public DateTime To { get; protected set; }
+        public DateTime From { get; private set; }
+        public DateTime To { get; private set; }
         public AbsenceType AbsenceType { get; set; }
         public string UserId { get; set; }
         public AppIdentityUser User { get; set; }
@@ -23,7 +23,6 @@ namespace DotNet2020.Domain._4.Models
         
         protected AbstractCalendarEntry(){}
 
-        #warning Если есть этот метод, то From и To нужно сделать приватные setter'ы
         public void ChangeDate(DateTime from, DateTime to)
         {
             if (to < from) throw new ArgumentException("You are trying set incorrect data period!" +
