02880d00430d48307c9d3db2ddcf174ae1b6309f
Royal Jackal <dimaorol@gmail.com>
2020-05-12 17:33:16 +0300

    added question count validation, prev attestation support

--- DotNet2020/DotNet2020.Domain.3/Services/QuestionService.cs
+++ DotNet2020/DotNet2020.Domain.3/Services/QuestionService.cs
@@ -18,17 +18,18 @@ namespace DotNet2020.Domain._3.Services
             _context = context;
         }
 
-        public List<QuestionModel> GetCompetencesAttestationQuestions(List<CompetencesModel> competences)
+        public List<QuestionModel> GetCompetencesAttestationQuestions(List<CompetencesModel> competences, out bool isValid)
         {
+            isValid = true;
             var result = new List<QuestionModel>();
             foreach (var competence in competences)
-                result.AddRange(GetOneCompetenceAttestationQuestions(competence));
+                result.AddRange(GetOneCompetenceAttestationQuestions(competence, out isValid));
             return result;
         }
 
-        public List<QuestionModel> GetOneCompetenceAttestationQuestions(CompetencesModel compentence)
+        public List<QuestionModel> GetOneCompetenceAttestationQuestions(CompetencesModel compentence, out bool isValid)
         {
-
+            isValid = true;
             var questionsIds = _context.Set<CompetenceQuestionsModel>().Where(x => x.CompetenceId == compentence.Id).Select(x => x.QuestionId).ToList();
             var questions = _context.Set<QuestionModel>().Where(x => questionsIds.Contains(x.Id)).ToList();
 
@@ -38,18 +39,48 @@ namespace DotNet2020.Domain._3.Services
                 GetRandomQuestion(questions, Medium),
                 GetRandomQuestion(questions, Hard)
             };
+            if (result.Count < 3)
+                isValid = false;
             return result;
         }
 
         public QuestionModel GetRandomQuestion(List<QuestionModel> questions, string difficulty)
         {
-            var rand = new Random();
+            var rnd = new Random();
             var complexityId = _context.Set<QuestionComplexityModel>().Where(x => x.Value == difficulty).Select(x => x.Id).FirstOrDefault();
             var neededQuestions = questions
                 .Where(q => q.ComplexityId == complexityId)
                 .ToList();
-            var number = rand.Next(0, neededQuestions.Count());
+            var number = rnd.Next(0, neededQuestions.Count());
             return neededQuestions[number];
+        }
+
+        public List<QuestionModel> GetPreviousAttestationQuestions(AttestationModel attestation)
+        {
+            var questions = _context.Set<QuestionModel>();
+            var attestationToAnswer = _context.Set<AttestationAnswerModel>();
+            var answers = _context.Set<AnswerModel>();
+
+            var answerIds = attestationToAnswer
+                .Where(x => x.AttestationId == attestation.Id)
+                .Select(x => x.AnswerId);
+
+            var neededQuestions = new List<QuestionModel>();
+            foreach (var id in answerIds)
+            {
+                var answer = answers.FirstOrDefault(a => a.AnswerId == id && a.IsRight == true);
+                if (answer == default) continue;
+                var question = questions.FirstOrDefault(q => q.Question == answer.Question);
+                if (question != default)
+                    neededQuestions.Add(question);
+            }
+            
+            var rnd = new Random();
+            var result = neededQuestions
+                .OrderBy(x => rnd.Next())
+                .Take(neededQuestions.Count / 2)
+                .ToList();
+            return result;
         }
     }
 }
