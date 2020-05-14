d5c9cb1df7aefaf87c7298f8c2048126c8325bb1
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-05-14 15:01:27 +0300

    Отпуск можно брать только на предстоящие даты

---
+++ DotNet2020/DotNet2020.Domain.4/Attributes/MoreThanCurrentDateAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNet2020.Domain.Attributes
+{
+    public class MoreThanCurrentDateAttribute : ValidationAttribute
+    {
+        public MoreThanCurrentDateAttribute()
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            var date = (DateTime)value;
+            return date.Date >= DateTime.Now.Date;
+        }
+    }
+}
