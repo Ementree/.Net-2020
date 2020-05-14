08b19175ccde81bf9f59868917463a8277edf167
Dmitry Chegodaev <cheg15032001@gmail.com>
2020-05-14 18:18:47 +0300

    1 (#28)
    Analyzers: Fluent Interface & Line Length (only highlights), Method params, Language Analyzer, Select Interpolation, Put/Delete Analyzer with CodeFix. distributed by folders. start [ToList and db query] Analyzer.
    
    Co-authored-by: shamilkhan
    Co-authored-by: MinahmetovaAlbina

--- DotNet2020/DotNet2020.Domain.1/DotNet2020.Domain._1/DotNet2020Domain_1Analyzer.cs
+++ DotNet2020/DotNet2020.Domain.1/DotNet2020.Domain._1/DotNet2020Domain_1Analyzer.cs
@@ -13,64 +13,43 @@ namespace DotNet2020.Domain._1
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class DotNet2020Domain_1Analyzer : DiagnosticAnalyzer
     {
-        public const string DiagnosticId = "PropertyDiagnosticId";
-        public const string CodeFixTitle = "Change modificator";
-
-        private const string Title = "Property encapsulation problem";
-        private const string MessageFormat = @"Property should have public get and protected set";
-        private const string Category = "Encapsulation";
-        private const string Description = @"Property should have public get and protected set";
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
-
-
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return ImmutableArray.Create(
+                    LineLengthAnalyzer.Rule,
+                    FluentAnalyzer.Rule,
+                    PropertyModifiersAnalyzer.Rule,
+                    MethodParamsAnalyzer.Rule,
+                    BooleanPropsNameAnalyzer.Rule,
+                    MethodBodyAnalyze.Rule,
+                    SimpleInterfaceAnalizer.Rule,
+                    EnumCheck.Rule,
+                    OneFileOneClass.Rule,
+                    LanguageAnalyzer.RussianRule,
+                    LanguageAnalyzer.EnglishRule,
+                    SelectAnalyzer.Rule,
+                    PutDeleteAnalyzer.DeleteRule,
+                    PutDeleteAnalyzer.PutRule);
+            }
+        }
 
         public override void Initialize(AnalysisContext context)
         {
-            context.RegisterSyntaxNodeAction(AnalyzeProperty, SyntaxKind.PropertyDeclaration);
-            context.RegisterSyntaxNodeAction(AnalyzeAttribute, SyntaxKind.AttributeList);
-        }
-
-        private static void AnalyzeProperty(SyntaxNodeAnalysisContext context)
-        {
-            var propertyNode = (PropertyDeclarationSyntax)context.Node;
-            var setAccessor = propertyNode.AccessorList.Accessors
-                .FirstOrDefault(acc => acc.IsKind(SyntaxKind.SetAccessorDeclaration));
-
-            if (propertyNode.Modifiers.Any(m => m.IsKind(SyntaxKind.PublicKeyword)) &&
-                (setAccessor is null || setAccessor.Modifiers.Any(m => m.IsKind(SyntaxKind.ProtectedKeyword))))
-            {
-                return;
-            }
-
-            context.ReportDiagnostic(Diagnostic.Create(Rule,
-                propertyNode.Identifier.GetLocation(),
-                propertyNode.Identifier.Text));
-        }
-
-        private static void AnalyzeAttribute(SyntaxNodeAnalysisContext context)
-        {
-            // var classNode = context.Node as ClassDeclarationSyntax;
-            var attributeListNode = (AttributeListSyntax)context.Node;
-
-            if (attributeListNode.CloseBracketToken.HasTrailingTrivia &&
-                attributeListNode.CloseBracketToken.TrailingTrivia.Any(t => t.IsKind(SyntaxKind.EndOfLineTrivia)))
-            {
-                return;
-            }
-
-            context.ReportDiagnostic(
-                Diagnostic.Create(
-                    new DiagnosticDescriptor(
-                        "AttributeDiagnosticId",
-                        "Attribute indentation problem",
-                        @"Every attribute should be on new line",
-                        "Formatting",
-                        DiagnosticSeverity.Warning,
-                        isEnabledByDefault: true,
-                        description: @"Every attribute should be on new line"),
-                    attributeListNode.GetLocation(),
-                    attributeListNode.GetText()));
+            context.RegisterSyntaxTreeAction(LineLengthAnalyzer.AnalyzeTree);
+            context.RegisterSyntaxNodeAction(FluentAnalyzer.Analyze, SyntaxKind.SimpleMemberAccessExpression);
+            context.RegisterSyntaxNodeAction(PropertyModifiersAnalyzer.Analyze, SyntaxKind.PropertyDeclaration);
+            context.RegisterSyntaxNodeAction(MethodParamsAnalyzer.Analyze, SyntaxKind.MethodDeclaration);
+            context.RegisterSyntaxNodeAction(OneFileOneClass.Analyze, SyntaxKind.NamespaceDeclaration);
+            context.RegisterSyntaxNodeAction(BooleanPropsNameAnalyzer.Analyze, SyntaxKind.PropertyDeclaration);
+            context.RegisterSyntaxNodeAction(SimpleInterfaceAnalizer.Analyze, SyntaxKind.InterfaceDeclaration);
+            context.RegisterSyntaxNodeAction(EnumCheck.Analyze, SyntaxKind.EnumDeclaration);
+            context.RegisterSyntaxNodeAction(MethodBodyAnalyze.Analyze, SyntaxKind.MethodDeclaration);
+            context.RegisterSyntaxNodeAction(LanguageAnalyzer.AnalyzeXML, SyntaxKind.SingleLineDocumentationCommentTrivia);
+            context.RegisterSyntaxTreeAction(LanguageAnalyzer.Analyze);
+            context.RegisterSyntaxNodeAction(SelectAnalyzer.Analyze, SyntaxKind.SimpleMemberAccessExpression);
+            context.RegisterSyntaxNodeAction(PutDeleteAnalyzer.Analyze, SyntaxKind.MethodDeclaration);
         }
     }
 }
---
+++ DotNet2020/DotNet2020.Domain.1/DotNet2020.Domain._1/Stylistic/FluentAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet2020.Domain._1
+{
+    /// <summary>
+    /// анализатор для проверки форматирования при использовании fluent-интерфейса =
+    /// каждый метод с новой строки.
+    /// </summary>
+    class FluentAnalyzer
+    {
+        public const string DiagnosticId = "FluentDiagnosticId";
+        public const string CodeFixTitle = "Put method on a new line";
+        const string Title = "violation of fluent-chain convention";
+        const string MessageFormat = "Fluent-chain convention: 1 method per 1 line";
+        const string Description = "Fluent-chain convention: 1 method per 1 line";
+        const string Category = "Stylistic";
+
+        public static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, 
+            DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public static void Analyze(SyntaxNodeAnalysisContext context)
+        {
+            var expression = context.Node as MemberAccessExpressionSyntax;
+            var dotToken = expression
+                .ChildTokens()
+                .FirstOrDefault(t => t.IsKind(SyntaxKind.DotToken));
+
+            if (expression.ChildNodes().Any(n => n.IsKind(SyntaxKind.InvocationExpression)) &&
+                dotToken != null &&
+                !dotToken.LeadingTrivia.Any(t => t.IsKind(SyntaxKind.WhitespaceTrivia)))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule,
+                    expression.GetLocation(),
+                    expression.GetText()));
+            }
+        }
+
+        public async Task<Solution> CodeFix()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
